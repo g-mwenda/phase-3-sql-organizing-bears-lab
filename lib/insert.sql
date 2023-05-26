e3e6918de596358af302698783e870f92112596a
Gabriel <gabrielmwenda0001@gmail.com>
2023-05-26 17:46:03 +0300

    Initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "green", "excited", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "brown", "mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "green", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 20, "M", "green", "mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "yellow", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 10, "M", "blue", "mean", 1);
